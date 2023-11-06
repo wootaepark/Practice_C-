8b37f9072708e457143bdb21b5059ceeba62c76e
wootaepark <pakdal99@gmail.com>
2023-11-06 19:42:47 +0900

    second_commit

---
+++ HelloWorld2/HelloWorld2/HelloWorld2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// (.net Framework) 를 사용함
+
+namespace HelloWorld2
+{
+    internal class HelloWorld2  // internal 을 붙이지 않아도 정상적으로 동작 (.net Core 에서 )
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("줄바꿈 있는 HelloWorld");
+            Console.Write("줄바꿈 없는 HelloWorld ");
+            Console.WriteLine("줄바꿈 있는 HelloWorld");
+
+        }
+    }
+}
